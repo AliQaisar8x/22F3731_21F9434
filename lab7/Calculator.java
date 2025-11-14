dcc53f3202653e7cc3fd8dc7253673d307978ecf
asadsabir01 <f219434@cfd.nu.edu.pk>
2025-11-14 02:23:16 -0800

    division and muliplication function added

--- lab7/Calculator.java
+++ lab7/Calculator.java
@@ -9,5 +9,18 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-  
+
+    // Multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divide two numbers
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero!");
+            return 0;
+        }
+        return a / b;
+    }
 }
