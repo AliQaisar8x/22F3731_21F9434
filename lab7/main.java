904bf0de6e2cb5bec234f6c983b9e27b0b4f3302
asadsabir01 <f219434@cfd.nu.edu.pk>
2025-11-14 02:26:10 -0800

    division and muliplication function added

--- lab7/main.java
+++ lab7/main.java
@@ -7,6 +7,8 @@ public class Main {
         int num2 = 5;
 
         System.out.println("Addition: " + calc.add(num1, num2));
-        System.out.println("Subtraction: " + calc.subtract(num1, num2));
+        System.out.println("Subtraction: " + calc.subtract(num1, num2));
+        System.out.println("Multiplication: " + calc.multiply(num1, num2));
+        System.out.println("Division: " + calc.divide(num1, num2));
     }
 }
